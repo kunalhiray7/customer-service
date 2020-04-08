316e4740a78a36ce551bf84b71a8c25bd05a58c6
Kunal Hire <kunal.hire@hexad.de>
2020-04-08 10:57:21 +0200

    KH | Added Swagger UI configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
